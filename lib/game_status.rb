e56eb212420549db23e616fb296f8f25460b90f4
Mustafa Taha <mtmax1994@gmail.com>
2016-10-17 07:07:56 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,53 +4,33 @@ def position_taken?(board, location)
 end
 
 # Define your WIN_COMBINATIONS constant
- WIN_COMBINATIONS =[
- [0,1,2],
- [3,4,5],
- [6,7,8],
- [2,5,8],
- [1,4,7],
- [0,3,6],
- [0,4,8],
- [2,4,6]]
- def won?(board)
-WIN_COMBINATIONS.detect do |combo|
- board[combo[0]] == board[combo[1]] && board[combo[1]] == board[combo[2]] && position_taken?(board,combo[0])
-
+WIN_COMBINATIONS=[
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+def won?(board)
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]]==board[combo[1]]&&board[combo[1]]==board[combo[2]]&&position_taken?(board,combo[0])
+  end
 end
+  def full?(board)
+    board.all?{|itm| itm == "X" || itm == "O"}
+  end
+  def draw?(board)
+    full?(board) && !won?(board)
 end
-def full?(board)
-  board.each do |as|
-    if as==" "
-      return false
-    end
-    end
-    return true
+def over?(board)
+  draw?(board)||won?(board)
 end
-def draw?(board)
-  if full?(board) && !won?(board)
-    return true
-  elsif full?(board) && !won?(board)
-    return false
-  elsif !full?(board) && won?(board)
-    return false
+def winner(board)
+  combo=won?(board)
+  if combo
+    board[combo[0]]
   end
-  end
-  def over?(board)
-    if draw?(board) && !won?(board)
-      return true
-    elsif won?(board) && !draw?(board)
-      return true
-    elsif !full?(board) && !won?(board)
-      return false
-    end
-  end
-  def winner(board)
-if draw?(board)
-  return nil
-elsif won?(board)==[0,4,8]
-  return "X"
-elsif won?(board)==[1,4,7]
-  return "O"
-    end
-  end
+end
